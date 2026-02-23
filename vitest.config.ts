1cca5d2b9e250c1130d90a807617788c021abd51
thisislokeshpawalia <this.is.lokesh.pawalia@gmail.com>
2026-02-23 18:10:25 +0530

    fix base path for github pages

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "/ishakulam/",
+  
   plugins: [react()],
   test: {
     environment: "jsdom",
